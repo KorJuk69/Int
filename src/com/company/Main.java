b4ee88210c8522a012081056b2b1820d9da4eaea
KorJuk69 <korjuk1@gmail.com>
2017-09-17 21:28:03 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,13 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int KolvoStrok = scanner.nextInt();
+        int NomerStroki = scanner.nextInt();
+
+        int NomerStr = (NomerStroki/KolvoStrok) + 1;
+        int NomerStrokiNaStr = NomerStroki - (NomerStr-1)*KolvoStrok;
+
+        System.out.print(NomerStr + " " + NomerStrokiNaStr);
 
     }
 }
