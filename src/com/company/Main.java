7ea8c4c4e595cfc3a254184a862b9f2d37541561
KorJuk69 <korjuk1@gmail.com>
2017-09-17 21:11:37 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        long chislo = (1 + n) * n / 2;
+
+        System.out.print(chislo);
 
     }
 }
