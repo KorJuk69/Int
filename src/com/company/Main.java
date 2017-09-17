7b45f10c941904b5e341c863b01178d3f02adecf
KorJuk69 <korjuk1@gmail.com>
2017-09-17 21:32:04 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,19 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int ch1 = scanner.nextInt();
+        int min1 = scanner.nextInt();
+        int sec1 = scanner.nextInt();
+        int ch2 = scanner.nextInt();
+        int min2 = scanner.nextInt();
+        int sec2 = scanner.nextInt();
+
+        sec1 = sec1 + min1*60 + ch1*3600;
+        sec2 = sec2 + min2*60 + ch2*3600;
+
+        int sec = sec2 - sec1;
+
+        System.out.print(sec);
 
     }
 }
